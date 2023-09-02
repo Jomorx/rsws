748e39f755ffc06e4ebe3be28ccf34f9ddd44662
huchanghao <2955568032@qq.com>
2023-09-03 02:43:11 +0800

    test router

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use std::net::TcpListener;
-
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    fs,
+};
 fn main() {
-    // 监听地址: 127.0.0.1:7878
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 }
+
+
+// --snip--
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let status_line = "HTTP/1.1 200 OK";
+    let response =|contents: String| {
+        let length = contents.len();
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}")};
+
+    if request_line == "GET / HTTP/1.1" {
+        print!("{request_line}");
+        let contents = fs::read_to_string("hello.html").unwrap();
+        stream.write_all(response(contents).as_bytes()).unwrap();
+    } else {
+        let contents = fs::read_to_string("info.html").unwrap();
+        stream.write_all(response(contents).as_bytes()).unwrap()
+    }
+}
+
