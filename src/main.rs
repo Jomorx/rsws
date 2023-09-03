f7a10214a6e7aa5d2f1997ed2021f4e9afa3996d
huchanghao <2955568032@qq.com>
2023-09-03 20:38:25 +0800

    add sleep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    fs,
+    fs, thread, time::Duration,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,18 +19,22 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let status_line = "HTTP/1.1 200 OK";
-    let response =|contents: String| {
+        let (status_line, filename) = match &request_line[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            "GET /sleep HTTP/1.1" => {
+                thread::sleep(Duration::from_secs(5));
+                ("HTTP/1.1 200 OK", "hello.html")
+            }
+            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        };
+    
+        let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}")};
-
-    if request_line == "GET / HTTP/1.1" {
-        print!("{request_line}");
-        let contents = fs::read_to_string("hello.html").unwrap();
-        stream.write_all(response(contents).as_bytes()).unwrap();
-    } else {
-        let contents = fs::read_to_string("info.html").unwrap();
-        stream.write_all(response(contents).as_bytes()).unwrap()
-    }
+    
+        let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+        stream.write_all(response.as_bytes()).unwrap();
+    
 }
 
